32729f9e939d82b9e007818d4fa156d308c64f99
theandersonn <theandersonn@gmail.com>
2022-05-02 17:14:08 -0300

    2-7: explorando o console

---
+++ 2-fundamentos/7-explorando-console/index.js
@@ -0,0 +1,29 @@
+/* exibindo mais de um valor no mesmo console
+--------------------------------------------- */
+const x = 10;
+const y = 'Jhon Doe';
+const z = [1,2];
+
+console.log(x,y,z);
+
+/* contagem de impressòes
+------------------------ */
+
+['a', 'b', 'c', 'd'].forEach(item => {
+  console.count(`O valor do item é: ${item}`);
+});
+
+console.count(`O valor de x é: ${x}, contagem`);
+console.count(`O valor de x é: ${x}, contagem`);
+console.count(`O valor de x é: ${x}, contagem`);
+console.count(`O valor de x é: ${x}, contagem`);
+
+/* variavel entre string
+------------------------ */
+console.log('O nome é %s, ele é programador', y);
+
+/* limpar o console
+------------------ */
+setTimeout(() => {
+  console.clear();
+}, 2000);
