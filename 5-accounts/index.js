346cb6f21b5fd8f7a66c5810a95cabd50f1d6062
theandersonn <theandersonn@gmail.com>
2022-05-10 22:02:40 -0300

    5-2: inicialização do sistema

--- 5-accounts/index.js
+++ 5-accounts/index.js
@@ -5,4 +5,22 @@ const chalk = require('chalk');
 // módulos internos
 const fs = require('fs');
 
-console.log('iniciamos o accounts');
+operation();
+
+function operation() {
+  inquirer.prompt([{
+    type: 'list',
+    name: 'action',
+    message: 'O que você deseja fazer?',
+    choices: [
+      'Criar conta',
+      'Consultar Saldo',
+      'Depositar',
+      'Sacar',
+      'Sair'
+    ]
+  }]).then((answer) => {
+    const action = answer['action'];
+    console.log(action);
+  }).catch(err => console.log(err));
+};
