5e90912fd6e0e5172476af84da2056d8219b5124
theandersonn <theandersonn@gmail.com>
2022-05-11 14:08:16 -0300

    5-3: criação da conta

--- 5-accounts/index.js
+++ 5-accounts/index.js
@@ -21,6 +21,14 @@ function operation() {
     ]
   }]).then((answer) => {
     const action = answer['action'];
-    console.log(action);
+    if (action === 'Criar conta') {
+      createAccount();
+    }
   }).catch(err => console.log(err));
 };
+
+// create an account
+function createAccount() {
+  console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
+  console.log(chalk.green('Defina as opções da sua conta a seguir'));
+}
