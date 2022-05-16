1a96d20f3f51143ea79dbd4873a252016317fcd3
theandersonn <theandersonn@gmail.com>
2022-05-16 09:55:17 -0300

    5-7: adição de saldo

--- 5-accounts/index.js
+++ 5-accounts/index.js
@@ -89,6 +89,21 @@ function deposit() {
     if (!checkAccount(accountName)) {
       return deposit();
     }
+
+    inquirer.prompt([
+      {
+        name: 'amount',
+        message: 'Quanto você deseja depositar'
+      },
+    ])
+    .then((answer) => {
+      const amount = answer['amount']
+
+      // add an amount
+      addAmount(accountName, amount);
+      operation();
+    })
+    .catch((err) => console.log(err))
   })
   .catch(err => console.log(err));
 }
@@ -100,4 +115,33 @@ function checkAccount(accountName) {
   }
 
   return true;
+};
+
+function addAmount(accountName, amount) {
+  const accountData = getAmount(accountName); 
+
+  if (!amount) {
+    console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'));
+    return deposit();
+  }
+
+  accountData.balance = parseFloat(amount) + parseFloat(accountData.balance);
+
+  fs.writeFileSync(
+    `accounts/${accountName}.json`,
+    JSON.stringify(accountData),
+    function (err) {
+      console.log(err);
+    }
+  );
+
+  console.log(chalk.green(`Foi depositado o valor de R$ ${amount} na sua conta!`));
+};
+
+function getAmount(accountName) {
+  const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+    encoding: 'utf8',
+    flag: 'r'
+  });
+  return JSON.parse(accountJSON);
 };
