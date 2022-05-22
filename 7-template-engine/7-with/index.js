e78d5685d9fa599fdee0b31ebc54d4cbf1931b62
theandersonn <theandersonn@gmail.com>
2022-05-22 13:45:37 -0300

    7-7: Utilizando o with

---
+++ 7-template-engine/7-with/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.get('/dashboard', (req, res) => {
+  const items = ['Item A', 'Item B', 'Item C'];
+  res.render('dashboard', { items });
+});
+
+app.get('/post', (req, res) => {
+  const post = {
+    title: 'Aprender Node.js',
+    category: 'JavaScript',
+    body: 'Este artigo vai te ajudar a aprender Node.js',
+    comments: 4
+  };
+
+  res.render('blogpost', { post });
+});
+
+app.get('/', (req, res) => {
+  const user = {
+    name: 'Jhon',
+    surname: 'Doe'
+  };
+  const auth = true;
+  
+  res.render('home', { user: user, auth });
+});
+
+app.listen(3000, () => {
+  console.log('App funcionando!');
+});
