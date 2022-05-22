482d420b5f96985e2c05eb4a242a4d76de836e87
theandersonn <theandersonn@gmail.com>
2022-05-22 00:05:17 -0300

    7-4: Condicionais no handlebars

---
+++ 7-template-engine/4-condicionais/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard');
+});
+
+app.get('/', (req, res) => {
+  const user = {
+    name: 'Jhon',
+    surname: 'Doe'
+  };
+  const auth = true;
+  
+  res.render('home', { user: user, auth });
+});
+
+app.listen(3000, () => {
+  console.log('App funcionando!');
+});
