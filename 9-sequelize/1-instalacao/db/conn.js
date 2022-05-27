cd56d82ed1c56dbff89054a96a34bd3776549ac9
theandersonn <theandersonn@gmail.com>
2022-05-27 17:59:47 -0300

    9-1: Conexão com sequelize

---
+++ 9-sequelize/1-instalacao/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('battisti-node', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+try {
+  sequelize.authenticate();
+  console.log('Conectamos com sucesso!');
+} catch (err) {
+  console.log('Não foi possivel conectar: ', error);
+}
+
+module.exports = sequelize;
