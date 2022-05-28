b73c9d7f4d39f7b67ff8bdc3d6e57eda7919ecdd
theandersonn <theandersonn@gmail.com>
2022-05-28 15:50:22 -0300

    atualiza nome da tabela

--- 9-sequelize/4-lendo-dados/models/User.js
+++ 9-sequelize/4-lendo-dados/models/User.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const db = require('../db/conn');
 
-const User = db.define('User', {
+const User = db.define('user', {
   name: {
     type: DataTypes.STRING,
     allowNull: false
