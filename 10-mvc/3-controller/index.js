f88b88202293820acf605f3105450a92ad0c66a0
theandersonn <theandersonn@gmail.com>
2022-05-30 15:25:10 -0300

    10-3: Criação do controller

---
+++ 10-mvc/3-controller/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const conn = require('./db/conn');
+const Task = require('./models/Task');
+
+const app = express();
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.use(
+  express.urlencoded({
+    extended:  true
+  })
+);
+app.use(express.json());
+app.use(express.static('public'));
+
+conn
+  .sync()
+  .then(() => { app.listen(3000) })
+  .catch((err) => console.log(err))
