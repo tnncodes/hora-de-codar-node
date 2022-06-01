294b9878002fcb1a75f59c50d7efeec5dd8b09b8
theandersonn <theandersonn@gmail.com>
2022-06-01 18:33:01 -0300

    Inserindo pensamentos no banco de dados

--- 11-toughts/controllers/ToughtController.js
+++ 11-toughts/controllers/ToughtController.js
@@ -13,4 +13,21 @@ module.exports = class ToughtController {
   static createTought(req, res) {
     res.render('toughts/create');
   };
+
+  static async createToughtSave(req, res) {
+    const tought = {
+      title: req.body.title,
+      UserId: req.session.userid
+    };
+
+    try {
+      await Tought.create(tought);
+      req.flash('message', 'Pensamento criado com sucesso!');
+      req.session.save(() => {
+        res.redirect('/toughts/dashboard');
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
