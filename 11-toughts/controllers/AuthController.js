f0c420383efb4fa8fc03b7cfda334c6a2bb2dbd9
theandersonn <theandersonn@gmail.com>
2022-06-01 14:33:51 -0300

    Finalizando autenticacao

--- 11-toughts/controllers/AuthController.js
+++ 11-toughts/controllers/AuthController.js
@@ -6,6 +6,35 @@ module.exports = class AuthController {
     res.render('auth/login');
   };
 
+  static async loginPost(req, res) {
+    const { email, password } = req.body;
+
+    // find user
+    const user = await User.findOne({ where: {email: email} });
+
+    if (!user) {
+      req.flash('message', 'Usuário não encontrado!');
+      res.render('auth/login');
+      return;
+    }
+
+    // check if passwords match
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+
+    if (!passwordMatch) {
+      req.flash('message', 'Senha inválida!');
+      res.render('auth/login');
+      return;
+    }
+
+    // initialize session
+    req.session.userid = user.id;
+    req.flash('message', 'Autenticação realizada com sucesso!');
+    req.session.save(() => {
+      res.redirect('/');
+    });
+  };
+
   static register(req, res) {
     res.render('auth/register');
   };
