3650e2843d9e5f8d783e66e56ebe71a4696cf476
theandersonn <theandersonn@gmail.com>
2022-06-01 23:03:39 -0300

    Preenchendo form de edicao com dados do pensamento

--- 11-toughts/controllers/ToughtController.js
+++ 11-toughts/controllers/ToughtController.js
@@ -66,4 +66,10 @@ module.exports = class ToughtController {
       console.log(error);
     }
   }
+
+  static async updateTought(req, res) {
+    const id = req.params.id;
+    const tought = await Tought.findOne({ where: {id: id}, raw: true });
+    res.render('toughts/edit', { tought });
+  }
 }
