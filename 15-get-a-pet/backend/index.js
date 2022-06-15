1616932b820a263cc74e2c57bd8a6f948a658886
theandersonn <theandersonn@gmail.com>
2022-06-15 18:50:25 -0300

    getAPet: backend - Criando controller e rotas de usuários

--- 15-get-a-pet/backend/index.js
+++ 15-get-a-pet/backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const UserRoutes = require('./routes/UserRoutes');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.static('public'));
 
 // Routes
+app.use('/users', UserRoutes);
 
 // Server
 app.listen(5000);
