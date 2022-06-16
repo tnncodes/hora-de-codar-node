e79e789336b7de2cf53e9e56ad6ff0e7bd95be14
theandersonn <theandersonn@gmail.com>
2022-06-15 23:22:22 -0300

    getAPet: backend - Pegando token do header

--- 15-get-a-pet/backend/controllers/UserController.js
+++ 15-get-a-pet/backend/controllers/UserController.js
@@ -1,6 +1,8 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const createUserToken = require('../helpers/create-user-token');
+const getToken = require('../helpers/get-token');
 module.exports = class UserController {
   static async register(req, res) {
     const { name, email, phone, password, confirmpassword } = req.body;
@@ -97,9 +99,12 @@ module.exports = class UserController {
 
   static async checkUser(req, res) {
     let currentUser;
-    console.log(req.headers.authorization);
+
     if (req.headers.authorization) {
-      
+      const token = getToken(req);
+      const decoded = jwt.verify(token, "nossosecret");
+      currentUser = await User.findById(decoded.id);
+      currentUser.password = undefined;
     } else  {
       currentUser = null;
     }
