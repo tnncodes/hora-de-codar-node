543c307a0e6216227c423d40cee1c5e01f506e59
theandersonn <theandersonn@gmail.com>
2022-06-18 18:26:59 -0300

    getAPet: backend - Salvando Pet no sistema

--- 15-get-a-pet/backend/routes/PetRoutes.js
+++ 15-get-a-pet/backend/routes/PetRoutes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const PetController = require('../controllers/PetController');
 
-router.post('/create', PetController.create);
+// middlewares
+const verifyToken = require('../helpers/verify-token');
+
+router.post('/create', verifyToken, PetController.create);
 
 module.exports = router;
