9fe3fa2b62b4e152aaa86ee283e090c3d0c77394
theandersonn <theandersonn@gmail.com>
2022-06-18 17:36:34 -0300

    getAPet: backend - Iniciando rotas de pets

--- 15-get-a-pet/backend/index.js
+++ 15-get-a-pet/backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const UserRoutes = require('./routes/UserRoutes');
+const PetRoutes = require('./routes/PetRoutes');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.static('public'));
 
 // Routes
 app.use('/users', UserRoutes);
+app.use('/pets', PetRoutes);
 
 // Server
 app.listen(5000);
