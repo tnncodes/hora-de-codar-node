dea38689ba00024a6f90aa596c82b0ff3ef99669
theandersonn <theandersonn@gmail.com>
2022-06-19 14:25:45 -0300

    getAPet: backend - Resgatando pet pelo id

--- 15-get-a-pet/backend/controllers/PetController.js
+++ 15-get-a-pet/backend/controllers/PetController.js
@@ -1,6 +1,7 @@
 const Pet = require('../models/Pet');
 const getToken = require('../helpers/get-token');
 const getUserByToken = require('../helpers/get-user-by-token');
+const ObjectId = require('mongoose').Types.ObjectId
 
 module.exports = class PetController {
   // create a pet
@@ -100,5 +101,26 @@ module.exports = class PetController {
     res.status(200).json({
       pets,
     })    
+  }
+
+  // get Pet By Id
+  static async getPetById(req, res) {
+    const id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+      res.status(422).json({ message: 'ID inválido.' });
+      return;
+    }
+
+    // check if pet exists
+    const pet = await Pet.findOne({ _id: id });
+    if (!pet) {
+      res.status(404).json({ message: 'Pet não encontrado.' });
+      return;
+    }
+
+    res.status(200).json({
+      pet: pet,
+    })
   }
 }
