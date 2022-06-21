f6b434cf4669b6d6ad1b1fa353ecede743dee9ec
theandersonn <theandersonn@gmail.com>
2022-06-21 18:48:17 -0300

    getAPet: backend - Concluindo a adoção do Pet

--- 15-get-a-pet/backend/controllers/PetController.js
+++ 15-get-a-pet/backend/controllers/PetController.js
@@ -258,4 +258,31 @@ module.exports = class PetController {
       message: `A visita foi agendada com sucesso, entre em contato com ${pet.user.name} pelo telefone ${pet.user.phone}`
     })
   }
+
+  // Conclude Adoption
+  static async concludeAdoption(req, res) {
+    const id = req.params.id;
+
+    // check if pet exists
+    const pet = await Pet.findOne({ _id: id });
+    if (!pet) {
+      res.status(404).json({ message: 'Pet não encontrado.' });
+      return;
+    }
+
+    // check if logged in user registered the pet
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+    if (pet.user._id.equals(user._id)) {
+      res.status(422).json({ message: 'Você não pode agendar uma visita com o seu próprio Pet!.' });
+      return;
+    }  
+
+    pet.available = false;
+    
+    await Pet.findByIdAndUpdate(id, pet);
+    res.status(200).json({
+      message: 'Parabéns! O ciclo de adoção foi finalizado com sucesso!'
+    })
+  }
 }
--- 15-get-a-pet/backend/routes/PetRoutes.js
+++ 15-get-a-pet/backend/routes/PetRoutes.js
@@ -13,5 +13,6 @@ router.get('/:id', PetController.getPetById);
 router.delete('/:id', verifyToken, PetController.removePetById);
 router.patch('/:id', verifyToken, imageUpload.array('images'), PetController.updatePet);
 router.patch('/schedule/:id', verifyToken, PetController.schedule);
+router.patch('/conclude/:id', verifyToken, PetController.concludeAdoption);
 
 module.exports = router;
