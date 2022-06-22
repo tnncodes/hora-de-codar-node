bafca481e2d519f4991487a6b98d469a84223b35
theandersonn <theandersonn@gmail.com>
2022-06-22 16:14:00 -0300

    getAPet: frontend - CSS na navbar e footer

--- 15-get-a-pet/frontend/src/components/layout/Footer.js
+++ 15-get-a-pet/frontend/src/components/layout/Footer.js
@@ -1,6 +1,8 @@
+import styles from './Footer.module.css'
+
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p><b>Get A Pet</b> &copy; 2021</p>
   </footer>
   )
