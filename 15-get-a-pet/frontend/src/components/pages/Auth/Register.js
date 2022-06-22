118d1a361c2947b27effa4a542b11dc3ec16b7cb
theandersonn <theandersonn@gmail.com>
2022-06-22 17:12:04 -0300

    getAPet: frontend - Criando a pagina e form de registro

--- 15-get-a-pet/frontend/src/components/pages/Auth/Register.js
+++ 15-get-a-pet/frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,53 @@
+import Input from "../../form/Input";
+
 function Register() {
+  function handleOnChange(e){}
   return (
     <section>
-      <h1>Register</h1>
+      <h1>Registrar</h1>
+      <form>
+        <Input 
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleOnChange}
+        />
+
+        <Input 
+          text="Telefone"
+          type="text"
+          name="name"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleOnChange}
+        />
+
+        <Input 
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite o seu e-mail"
+          handleOnChange={handleOnChange}
+        />
+
+        <Input 
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleOnChange}
+        />
+
+        <Input 
+          text="Confirmação de senha"
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm a sua senha"
+          handleOnChange={handleOnChange}
+        />
+
+        <input type='submit' value='cadastrar' />
+      </form>  
     </section>
   )
 }
