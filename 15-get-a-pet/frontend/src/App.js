5f8bb681b7a2b83c65586abf2d07da8720371ea0
theandersonn <theandersonn@gmail.com>
2022-06-22 16:33:14 -0300

    getAPet: frontend - Criando componente de container

--- 15-get-a-pet/frontend/src/App.js
+++ 15-get-a-pet/frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 /* components */
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer'
+import Container from './components/layout/Container'
 
 /* pages */
 import Login from './components/pages/Auth/Login';
@@ -13,11 +14,13 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Container>
       <Footer />
     </Router>
   );
