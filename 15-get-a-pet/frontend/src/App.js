0e7af699679180c0bde3f194d021193e1a6a3353
theandersonn <theandersonn@gmail.com>
2022-06-22 15:18:05 -0300

    getAPet: frontend - Inserindo o React Router

--- 15-get-a-pet/frontend/src/App.js
+++ 15-get-a-pet/frontend/src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/pages/Auth/Login';
+import Register from './components/pages/Auth/Register'
+import Home from './components/pages/Home'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
