8d24e0b5c7f09ef5ec8c95c203120f28a296c2a8
theandersonn <theandersonn@gmail.com>
2022-06-24 13:59:10 -0300

    getAPet: frontend - Criando formulario de login

--- 15-get-a-pet/frontend/src/components/pages/Auth/Login.js
+++ 15-get-a-pet/frontend/src/components/pages/Auth/Login.js
@@ -1,7 +1,40 @@
+import { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import Input from '../../form/Input';
+
+import styles from '../../form/Form.module.css';
+
+/* context */
+import { Context } from '../../../context/UserContext';
+
 function Login() {
+  function handleChange(e){}
   return (
-    <section>
+    <section className={styles.form_container}>
       <h1>Login</h1>
+      <form>
+        <Input 
+          text='E-mail'
+          type='email'
+          name='email'
+          placeholder='Digite o seu e-mail'
+          handleOnChange={handleChange}
+        />
+
+        <Input 
+          text='Senha'
+          type='password'
+          name='password'
+          placeholder='Digite a sua senha'
+          handleOnChange={handleChange}
+        />
+
+        <input type='submit' value='Entrar' />
+
+        <p>
+          Não tem conta? <Link to='/register'>clique aqui.</Link>
+        </p>
+      </form>
     </section>
   )
 }
