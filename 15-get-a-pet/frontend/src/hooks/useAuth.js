1a124079605a275eda3176a74b54f946e7cd3206
theandersonn <theandersonn@gmail.com>
2022-06-24 18:35:47 -0300

    getAPet: frontend - Função de login

--- 15-get-a-pet/frontend/src/hooks/useAuth.js
+++ 15-get-a-pet/frontend/src/hooks/useAuth.js
@@ -42,6 +42,24 @@ export default function useAuth() {
     navigate('/');
   }
 
+  async function login(user) {
+    let msgText = 'Login realizado com sucesso!';
+    let msgType = 'success';
+
+    try {
+      const data = await api.post('/users/login', user).then((response) => {
+        return response.data;
+      });
+
+      await authUser(data);
+    } catch (error) {
+      msgText = error.response.data.message;
+      msgType = 'error';
+    }
+
+    setFlashMessage(msgText, msgType);
+  }
+
   function logout() {
     const msgText = 'Logout realizado com sucesso!';
     const msgType = 'success';
@@ -54,5 +72,5 @@ export default function useAuth() {
     setFlashMessage(msgText, msgType);
   }
 
-  return { authenticated, register, logout };
+  return { authenticated, register, logout, login };
 }
