89cac6558d44be667cb61f2232d51f3e7e294ecf
theandersonn <theandersonn@gmail.com>
2022-06-25 14:37:04 -0300

    getAPet: frontend - Atualização do usuario

--- 15-get-a-pet/frontend/src/components/pages/User/Profile.js
+++ 15-get-a-pet/frontend/src/components/pages/User/Profile.js
@@ -1,14 +1,46 @@
 import { useState, useEffect } from 'react';
 import api from '../../../utils/api';
 
+import Input from '../../form/Input';
+import useFlashMessage from '../../../hooks/useFlashMessage';
+
 import styles from './Profile.module.css';
 import formStyles from '../../form/Form.module.css';
-
-import Input from '../../form/Input';
 
 function Profile() {
   const [user, setUser] = useState({});
   const [token] = useState(localStorage.getItem('token') || '');
+  const { setFlashMessage } = useFlashMessage();
+
+  function onFileChange(e){
+    setUser({ ...user, [e.target.name]: e.target.files[0] });
+  }
+
+  function handleChange(e){
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    let msgType = 'success';
+    const formData = new FormData();
+    await Object.keys(user).forEach((key) => formData.append(key, user[key]));
+
+    const data = await api.patch(`/users/edit/${user._id}`, formData, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+        'Content-Type': 'multipart/form-data',
+      }
+    }).then((response) => {
+      return response.data;
+    }).catch((err) => {
+      msgType = 'error';
+      return err.response.data
+    });
+
+    setFlashMessage(data.message, msgType);
+  };
 
   useEffect(() => {
     api
@@ -22,9 +54,6 @@ function Profile() {
       })
   }, [token]);
 
-  function onFileChange(){}
-  function handleChange(){}
-
   return (
     <section>
       <div className={styles.profile_header}>
@@ -32,7 +61,7 @@ function Profile() {
         <p>Preview Imagem</p>
       </div>
 
-      <form className={formStyles.form_container}>
+      <form className={formStyles.form_container} onSubmit={handleSubmit}>
         <Input
           text="Imagem"
           type="file"
