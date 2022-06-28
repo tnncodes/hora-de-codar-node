d7ac2bc71205c8f815a6885dac2ee4dd08e51f1f
theandersonn <theandersonn@gmail.com>
2022-06-28 14:27:15 -0300

    getAPet: frontend - Conclusão da tela de adicionar pet

--- 15-get-a-pet/frontend/src/components/form/PetForm.js
+++ 15-get-a-pet/frontend/src/components/form/PetForm.js
@@ -25,8 +25,7 @@ function PetForm({ handleSubmit, petData, btnText }) {
 
   function submit(e) {
     e.preventDefault();
-    console.log(pet)
-    // handleSubmit(pet);
+    handleSubmit(pet);
   }
 
   return (
