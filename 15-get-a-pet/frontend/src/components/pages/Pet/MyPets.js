e13ee606debeb08eb738be264501f76718955eb8
theandersonn <theandersonn@gmail.com>
2022-06-29 14:21:43 -0300

    getAPet: frontend - Exibindo os pets na dashboard

--- 15-get-a-pet/frontend/src/components/pages/Pet/MyPets.js
+++ 15-get-a-pet/frontend/src/components/pages/Pet/MyPets.js
@@ -6,6 +6,8 @@ import RoundedImage from '../../layout/RoundedImage';
 /* hooks */
 import useFlashMessage from '../../../hooks/useFlashMessage';
 
+import styles from './Dashboard.module.css';
+
 function MyPets() {
   const [pets, setPets] = useState([]);
   const [token] = useState(localStorage.getItem('token') || '');
@@ -24,14 +26,36 @@ function MyPets() {
 
   return (
     <section>
-      <div>
+      <div className={styles.petslist_header}>
         <h1>MyPets</h1>
         <Link to='/pet/add'>Cadastrar Pet</Link>
       </div>
-      <div>
-        {pets.length === 0 && <p>Não há Pets cadastrados</p>}.
-        {pets.length > 0 && <p>Meus Pets cadastrados.</p>}
-        </div> 
+      <div className={styles.petslist_container}>
+        {pets.length === 0 && <p>Não há Pets cadastrados</p>}
+        {pets.length > 0 && 
+          pets.map((pet) => (
+            <div key={pet._id} className={styles.petlist_row}>
+              <RoundedImage
+                src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
+                alt={pet.name}
+                width="px75"
+              />
+              <span className='bold'>{pet.name}</span>
+              <div className={styles.actions}>
+                {pet.available ? (
+                  <>
+                    {pet.adopter && <button className={styles.conclude_btn}>Concluir adoção</button>}
+                    <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
+                    <button>Excluir</button>
+                  </>
+                ) : (
+                  <p>Pet já adotado</p>
+                )}
+              </div>
+            </div>
+          ))
+        }  
+      </div> 
     </section>
   )
 };
