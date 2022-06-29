95117790a9deb5bee05ab1e10c9515d6a3c09762
theandersonn <theandersonn@gmail.com>
2022-06-29 14:41:04 -0300

    getAPet: frontend - Removendo o pet do sistema

--- 15-get-a-pet/frontend/src/components/pages/Pet/MyPets.js
+++ 15-get-a-pet/frontend/src/components/pages/Pet/MyPets.js
@@ -24,6 +24,24 @@ function MyPets() {
     })
   }, [token])
 
+  async function removePet(id) {
+    let msgType = 'success';
+    const data = await api.delete(`/pets/${id}`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`
+      }
+    }).then((response) => {
+      const updatedPets = pets.filter((pet) => pet._id !== id);
+      setPets(updatedPets);
+      return response.data;
+    }).catch((err) => {
+      msgType = 'error';
+      return err.response.data;
+    })
+
+    setFlashMessage(data.message, msgType);
+  }
+
   return (
     <section>
       <div className={styles.petslist_header}>
@@ -46,7 +64,7 @@ function MyPets() {
                   <>
                     {pet.adopter && <button className={styles.conclude_btn}>Concluir adoção</button>}
                     <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
-                    <button>Excluir</button>
+                    <button onClick={() => removePet(pet._id)}>Excluir</button>
                   </>
                 ) : (
                   <p>Pet já adotado</p>
