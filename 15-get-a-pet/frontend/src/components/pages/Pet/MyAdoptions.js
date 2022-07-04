9c546a7f284c93e7232a2cb88f2abce6cbc7250a
theandersonn <theandersonn@gmail.com>
2022-07-04 20:16:37 -0300

    getAPet: frontend - Finalizando a página de adocoes

--- 15-get-a-pet/frontend/src/components/pages/Pet/MyAdoptions.js
+++ 15-get-a-pet/frontend/src/components/pages/Pet/MyAdoptions.js
@@ -20,7 +20,42 @@ function MyAdoptions() {
       })
   }, [token]);
 
-  return <p>My Adoptions</p>;
+  return (
+    <section>
+      <div className={styles.petslist_header}>
+        <h1>Minhas adoções</h1>
+      </div>
+      <div className={styles.petslist_container}>
+        {pets.length > 0 &&
+          pets.map((pet) => (
+            <div key={pet._id} className={styles.petlist_row}>
+              <RoundedImage
+                src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
+                alt={pet.name}
+                width="px75"
+              />
+              <span className="bold">{pet.name}</span>
+              <div className={styles.contacts}>
+                <p>
+                  <span className="bold">Ligue para:</span> {pet.user.phone}
+                </p>
+                <p>
+                  <span className="bold">Fale com:</span> {pet.user.name}
+                </p>
+              </div>
+              <div className={styles.actions}>
+                {pet.available ? (
+                  <p>Adoção em processo</p>
+                ) : (
+                  <p>Parabéns por concluir a adoção</p>
+                )}
+              </div>
+            </div>
+          ))}
+        {pets.length === 0 && <p>Ainda não há pets adotados!</p>}
+      </div>
+    </section>
+  )
 };
 
 export default MyAdoptions;
